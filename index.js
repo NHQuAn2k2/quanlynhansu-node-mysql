8da48ae1df77a96783cc56a7c50ffe0c88c92dd4
quan <mhoangquan0@gmail.com>
2024-03-26 14:24:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,372 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+const mysql = require("mysql2");
+const port = 3001;
+app.use(cors(), express.json());
+// mysql------------------------------
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "quanlynhansu",
+});
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+//-------------------------------------
+// dang nhap
+app.post("/login", (req, res) => {
+  const { username, email, password } = req.body;
+  con.query(
+    "select * from nguoidung where username = ? and email = ?",
+    [username, email],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        return res.status(400).json({ message: "thong tin sai!" });
+      } else {
+        const compare = password === result[0].matkhau;
+        if (!compare)
+          return res.status(400).json({ message: "mat khau khong dung!" });
+        const token = jwt.sign(
+          { manguoidung: result[0].manguoidung, username: result[0].username },
+          "jwtjsonwebtoken",
+          { expiresIn: "1h" }
+        );
+        return res
+          .status(200)
+          .json({ message: "dang nhap thanh cong!", token });
+      }
+    }
+  );
+});
+//--------------------------------------------
+// nhan su
+app.get("/danh-sach-nhan-vien", (req, res) => {
+  con.query("select * from nhanvien", (err, result) => {
+    if (err) throw err;
+    if (result.length === 0) {
+      return res.status(200).json({ data: [] });
+    } else {
+      return res.status(200).json({ data: result });
+    }
+  });
+});
+app.get("/chi-tiet-nhan-vien/:id", (req, res) => {
+  const { id } = req.params;
+  con.query(
+    "select * from nhanvien join phongban on nhanvien.maphongban = phongban.maphongban join khoa on nhanvien.makhoa = khoa.makhoa where manhanvien = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        return res.status(400).json({ message: "nhan vien khong ton tai!" });
+      } else {
+        let newResult = {};
+        if (result[0].tenkhoa !== "khong co") {
+          newResult = { ...result[0], donvi: result[0].tenkhoa };
+        }
+        if (result[0].tenphongban !== "khong co") {
+          newResult = { ...result[0], donvi: result[0].tenphongban };
+        }
+        return res.status(200).json({ data: newResult });
+      }
+    }
+  );
+});
+app.post("/them-nhan-vien", (req, res) => {
+  const {
+    hoten,
+    namsinh,
+    gioitinh,
+    cccd,
+    diachi,
+    dienthoai,
+    email,
+    vitri,
+    bangcap,
+    trinhdohocvan,
+    kinhnghiem,
+    trinhdochuyenmon,
+    makhoa,
+    maphongban,
+    linhvuc,
+  } = req.body;
+  con.query(
+    "insert into nhanvien (hoten, namsinh, gioitinh, cccd, diachi, dienthoai, email, vitri, bangcap, trinhdohocvan, kinhnghiem, trinhdochuyenmon, makhoa, maphongban, linhvuc) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      hoten,
+      namsinh,
+      gioitinh,
+      cccd,
+      diachi,
+      dienthoai,
+      email,
+      vitri,
+      bangcap,
+      trinhdohocvan,
+      kinhnghiem,
+      trinhdochuyenmon,
+      makhoa,
+      maphongban,
+      linhvuc,
+    ],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "them thanh cong" });
+    }
+  );
+});
+app.post("/xoa-nhan-vien/:id", (req, res) => {
+  const { id } = req.params;
+  con.query(
+    "delete from nhanvien where manhanvien = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "xoa thanh cong!" });
+    }
+  );
+});
+app.post("/sua-nhan-vien/:id", (req, res) => {
+  const {
+    hoten,
+    namsinh,
+    gioitinh,
+    cccd,
+    diachi,
+    dienthoai,
+    email,
+    vitri,
+    bangcap,
+    trinhdohocvan,
+    kinhnghiem,
+    trinhdochuyenmon,
+    makhoa,
+    maphongban,
+    linhvuc,
+  } = req.body;
+  const { id } = req.params;
+  con.query(
+    "update nhanvien set hoten = ?, namsinh = ?, gioitinh = ?, cccd = ?, diachi = ?, dienthoai = ?, email = ?, vitri = ?, bangcap = ?, trinhdohocvan = ?, kinhnghiem = ?, trinhdochuyenmon = ?, makhoa = ?, maphongban = ?, linhvuc = ? where manhanvien = ?",
+    [
+      hoten,
+      namsinh,
+      gioitinh,
+      cccd,
+      diachi,
+      dienthoai,
+      email,
+      vitri,
+      bangcap,
+      trinhdohocvan,
+      kinhnghiem,
+      trinhdochuyenmon,
+      makhoa,
+      maphongban,
+      linhvuc,
+      id,
+    ],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "sua thanh cong!" });
+    }
+  );
+});
+//--------------------------------------------
+// phong ban
+app.get("/danh-sach-phong-ban", (req, res) => {
+  con.query("select * from phongban", (err, result) => {
+    if (err) throw err;
+    return res.status(200).json({ data: result });
+  });
+});
+app.get("/chi-tiet-phong-ban/:id", (req, res) => {
+  const { id } = req.params;
+  con.query(
+    "select * from phongban where maphongban = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      con.query(
+        "select * from nhanvien join phongban on nhanvien.maphongban = phongban.maphongban where nhanvien.maphongban = ?",
+        [result[0].maphongban],
+        (err, result2) => {
+          if (err) throw err;
+          return res
+            .status(200)
+            .json({ data: { ...result[0], nhanvien: result2 } });
+        }
+      );
+    }
+  );
+});
+app.post("/them-phong-ban", (req, res) => {
+  const { tenphongban } = req.body;
+  con.query(
+    "insert into phongban (tenphongban) values (?)",
+    [tenphongban],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "them thanh cong!" });
+    }
+  );
+});
+//--------------------------------------------
+// khoa
+app.get("/danh-sach-khoa", (req, res) => {
+  con.query("select * from khoa", (err, result) => {
+    if (err) throw err;
+    return res.status(200).json({ data: result });
+  });
+});
+app.get("/chi-tiet-khoa/:id", (req, res) => {
+  const { id } = req.params;
+  con.query("select * from khoa where makhoa = ?", [id], (err, result) => {
+    if (err) throw err;
+    con.query(
+      "select * from nhanvien join khoa on nhanvien.makhoa = khoa.makhoa where nhanvien.makhoa = ?",
+      [result[0].makhoa],
+      (err, result2) => {
+        if (err) throw err;
+        return res
+          .status(200)
+          .json({ data: { ...result[0], nhanvien: result2 } });
+      }
+    );
+  });
+});
+app.post("/them-khoa", (req, res) => {
+  const { tenkhoa } = req.body;
+  con.query(
+    "insert into khoa (tenkhoa) values (?)",
+    [tenkhoa],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "them thanh cong!" });
+    }
+  );
+});
+// cong tac
+app.post("/them-cong-tac", (req, res) => {
+  const {
+    manhanvien,
+    ngaybatdau,
+    ngayketthuc,
+    namhoc,
+    hocky,
+    noidung,
+    trangthai,
+  } = req.body;
+  con.query(
+    "insert into congtac (manhanvien, ngaybatdau, ngayketthuc, namhoc, hocky, noidung, trangthai) values (?, ?, ?, ?, ?, ?, ?)",
+    [manhanvien, ngaybatdau, ngayketthuc, namhoc, hocky, noidung, trangthai],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "thanh cong!" });
+    }
+  );
+});
+app.get("/danh-sach-cong-tac", (req, res) => {
+  con.query(
+    "select * from congtac join nhanvien on congtac.manhanvien = nhanvien.manhanvien",
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ data: result });
+    }
+  );
+});
+app.post("/xoa-cong-tac/:id", (req, res) => {
+  const { id } = req.params;
+  con.query("delete from congtac where macongtac = ?", [id], (err, result) => {
+    if (err) throw err;
+    return res.status(200).json({ message: "xoa thanh cong!" });
+  });
+});
+app.post("/sua-cong-tac/:id", (req, res) => {
+  const { id } = req.params;
+  const { ngaybatdau, ngayketthuc, namhoc, hocky, noidung, trangthai } =
+    req.body;
+  con.query(
+    "update congtac set ngaybatdau = ?, ngayketthuc = ?, namhoc = ?, hocky = ?, noidung = ?, trangthai = ? where macongtac = ?",
+    [ngaybatdau, ngayketthuc, namhoc, hocky, noidung, trangthai, id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "sua thanh cong!" });
+    }
+  );
+});
+app.get("/chi-tiet-cong-tac/:id", (req, res) => {
+  const { id } = req.params;
+  con.query(
+    "select * from congtac join nhanvien on congtac.manhanvien = nhanvien.manhanvien where macongtac = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ data: result[0] });
+    }
+  );
+});
+//---------------------------------------------
+// nghi phep
+app.post("/them-nghi-phep", (req, res) => {
+  const { manhanvien, ngaybatdau, ngayketthuc, lydo, trangthai } = req.body;
+  con.query(
+    "insert into nghiphep (manhanvien, ngaybatdau, ngayketthuc, lydo, trangthai) values (?, ?, ?, ?, ?)",
+    [manhanvien, ngaybatdau, ngayketthuc, lydo, trangthai],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "them thanh cong!" });
+    }
+  );
+});
+app.get("/danh-sach-nghi-phep", (req, res) => {
+  con.query(
+    "select * from nghiphep join nhanvien on nghiphep.manhanvien = nhanvien.manhanvien",
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ data: result });
+    }
+  );
+});
+app.post("/xoa-nghi-phep/:id", (req, res) => {
+  const { id } = req.params;
+  con.query(
+    "delete from nghiphep where manghiphep = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "xoa thanh cong!" });
+    }
+  );
+});
+app.post("/sua-nghi-phep/:id", (req, res) => {
+  const { id } = req.params;
+  const { ngaybatdau, ngayketthuc, lydo, trangthai } = req.body;
+  con.query(
+    "update nghiphep set ngaybatdau = ?, ngayketthuc = ?, lydo = ?, trangthai = ? where manghiphep = ?",
+    [ngaybatdau, ngayketthuc, lydo, trangthai, id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ message: "sua thanh cong!" });
+    }
+  );
+});
+app.get("/chi-tiet-nghi-phep/:id", (req, res) => {
+  const { id } = req.params;
+  con.query(
+    "select * from nghiphep join nhanvien on nghiphep.manhanvien = nhanvien.manhanvien where manghiphep = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({ data: result[0] });
+    }
+  );
+});
+//---------------------------------------------
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
---
+++ quanlynhansu.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th3 22, 2024 lúc 09:58 AM
+-- Phiên bản máy phục vụ: 8.2.0
+-- Phiên bản PHP: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanlynhansu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `congtac`
+--
+
+DROP TABLE IF EXISTS `congtac`;
+CREATE TABLE IF NOT EXISTS `congtac` (
+  `macongtac` int NOT NULL AUTO_INCREMENT,
+  `manhanvien` int NOT NULL,
+  `ngaybatdau` date NOT NULL,
+  `ngayketthuc` date NOT NULL,
+  `namhoc` varchar(255) NOT NULL,
+  `hocky` varchar(255) NOT NULL,
+  `noidung` varchar(255) NOT NULL,
+  `trangthai` varchar(255) NOT NULL,
+  PRIMARY KEY (`macongtac`),
+  KEY `FK_congtac_manhanvien` (`manhanvien`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `congtac`
+--
+
+INSERT INTO `congtac` (`macongtac`, `manhanvien`, `ngaybatdau`, `ngayketthuc`, `namhoc`, `hocky`, `noidung`, `trangthai`) VALUES
+(2, 13, '2023-05-12', '2024-07-12', '2023 - 2024', 'hoc ky 1', 'lap trinh web', 'dang lam');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khoa`
+--
+
+DROP TABLE IF EXISTS `khoa`;
+CREATE TABLE IF NOT EXISTS `khoa` (
+  `makhoa` int NOT NULL AUTO_INCREMENT,
+  `tenkhoa` varchar(255) NOT NULL,
+  PRIMARY KEY (`makhoa`),
+  UNIQUE KEY `makhoa` (`makhoa`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `khoa`
+--
+
+INSERT INTO `khoa` (`makhoa`, `tenkhoa`) VALUES
+(-1, 'khong co'),
+(1, 'Khoa Cong Nghe Thong Tin'),
+(2, 'Khoa Ky Thuat Cong Trinh');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nghiphep`
+--
+
+DROP TABLE IF EXISTS `nghiphep`;
+CREATE TABLE IF NOT EXISTS `nghiphep` (
+  `manghiphep` int NOT NULL AUTO_INCREMENT,
+  `manhanvien` int NOT NULL,
+  `ngaybatdau` date NOT NULL,
+  `ngayketthuc` date NOT NULL,
+  `lydo` varchar(255) NOT NULL,
+  `trangthai` varchar(255) NOT NULL,
+  PRIMARY KEY (`manghiphep`),
+  KEY `FK_nghiphep_manhanvien` (`manhanvien`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nguoidung`
+--
+
+DROP TABLE IF EXISTS `nguoidung`;
+CREATE TABLE IF NOT EXISTS `nguoidung` (
+  `manguoidung` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `matkhau` varchar(255) NOT NULL,
+  `vaitro` varchar(255) NOT NULL,
+  PRIMARY KEY (`manguoidung`),
+  UNIQUE KEY `username` (`username`,`email`),
+  UNIQUE KEY `manguoidung` (`manguoidung`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nguoidung`
+--
+
+INSERT INTO `nguoidung` (`manguoidung`, `username`, `email`, `matkhau`, `vaitro`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin123456', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhanvien`
+--
+
+DROP TABLE IF EXISTS `nhanvien`;
+CREATE TABLE IF NOT EXISTS `nhanvien` (
+  `manhanvien` int NOT NULL AUTO_INCREMENT,
+  `hoten` varchar(255) NOT NULL,
+  `namsinh` date NOT NULL,
+  `gioitinh` varchar(255) NOT NULL,
+  `cccd` int NOT NULL,
+  `diachi` varchar(255) NOT NULL,
+  `dienthoai` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `vitri` varchar(255) NOT NULL,
+  `bangcap` varchar(255) NOT NULL,
+  `trinhdohocvan` varchar(255) NOT NULL,
+  `kinhnghiem` varchar(255) NOT NULL,
+  `trinhdochuyenmon` varchar(255) NOT NULL,
+  `makhoa` int NOT NULL,
+  `maphongban` int NOT NULL,
+  `linhvuc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`manhanvien`),
+  UNIQUE KEY `manhanvien` (`manhanvien`),
+  KEY `FK_nhanvien_makhoa` (`makhoa`),
+  KEY `FK_nhanvien_maphongban` (`maphongban`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhanvien`
+--
+
+INSERT INTO `nhanvien` (`manhanvien`, `hoten`, `namsinh`, `gioitinh`, `cccd`, `diachi`, `dienthoai`, `email`, `vitri`, `bangcap`, `trinhdohocvan`, `kinhnghiem`, `trinhdochuyenmon`, `makhoa`, `maphongban`, `linhvuc`) VALUES
+(12, 'Nguyen Van A', '2002-05-26', 'Nam', 123456, 'TP.HCM', 123456, 'nguyenvana@gmail.com', 'Giang Vien', 'Thac Si', '12/12', 'Lap trinh huong doi tuong', 'DH Bach Khoa TP.HCM', -1, 1, 'khoa hoc may tinh'),
+(13, 'Nguyen Van A123', '1999-12-23', 'Nam', 123456, 'TP.HCM', 123456, 'nguyenvana@gmail.com', 'Giang Vien', 'Thac Si', '12/12', 'Lap trinh huong doi tuong', 'DH Bach Khoa TP.HCM', -1, 1, 'khoa hoc may tinh'),
+(14, 'Nguyen Van A456', '1999-04-23', 'Nam', 123456, 'TP.HCM', 123456, 'nguyenvana@gmail.com', 'Giang Vien', 'Thac Si', '12/12', 'Lap trinh huong doi tuong', 'DH Bach Khoa TP.HCM', 1, -1, 'khoa hoc may tinh');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phongban`
+--
+
+DROP TABLE IF EXISTS `phongban`;
+CREATE TABLE IF NOT EXISTS `phongban` (
+  `maphongban` int NOT NULL AUTO_INCREMENT,
+  `tenphongban` varchar(255) NOT NULL,
+  PRIMARY KEY (`maphongban`),
+  UNIQUE KEY `maphongban` (`maphongban`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `phongban`
+--
+
+INSERT INTO `phongban` (`maphongban`, `tenphongban`) VALUES
+(-1, 'khong co'),
+(1, 'Phong Dao Tao'),
+(3, 'Phong Hanh Chinh Quan Tri'),
+(4, 'Phong Ke Hoach Tai Chinh'),
+(6, 'Phong Cong Tac Sinh Vien');
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `congtac`
+--
+ALTER TABLE `congtac`
+  ADD CONSTRAINT `FK_congtac_manhanvien` FOREIGN KEY (`manhanvien`) REFERENCES `nhanvien` (`manhanvien`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `nghiphep`
+--
+ALTER TABLE `nghiphep`
+  ADD CONSTRAINT `FK_nghiphep_manhanvien` FOREIGN KEY (`manhanvien`) REFERENCES `nhanvien` (`manhanvien`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD CONSTRAINT `FK_nhanvien_makhoa` FOREIGN KEY (`makhoa`) REFERENCES `khoa` (`makhoa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_nhanvien_maphongban` FOREIGN KEY (`maphongban`) REFERENCES `phongban` (`maphongban`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
